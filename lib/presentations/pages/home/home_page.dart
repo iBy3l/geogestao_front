cee2978af81e9f1788786e7c70a99dcd3460096a
iby3l <looqgabriel@gmail.com>
2025-12-31 11:48:57 -0300

    feat: refactor map integration and remove unused components

--- lib/presentations/pages/home/home_page.dart
+++ lib/presentations/pages/home/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geogestao_front/core/consts/strings_const.dart';
-import 'package:geogestao_front/shared/map/geo_map.dart';
-import 'package:geogestao_front/shared/map/geo_point.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:geogestao_front/core/core.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
 import '/presentations/pages/home/controllers/home_controller.dart';
 import '/shared/shared.dart';
@@ -13,31 +11,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final points = List.generate(5000, (i) {
-      final status = switch (i % 4) {
-        0 => ClientStatus.ativo,
-        1 => ClientStatus.atrasado,
-        2 => ClientStatus.emVisita,
-        _ => ClientStatus.inativo,
-      };
-
-      return GeoPoint(id: 'cliente_$i', position: LatLng(-23.55 + (i % 200) * 0.001, -46.63 + (i % 200) * 0.001), status: status);
-    });
+    debugPrint('key map => ${StringsConst().mapbox}');
     return ResponsiveLayout(
       metas: controller.meta,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            GeoMap(
-              mapboxAccessToken: StringsConst().mapbox,
-              height: 520,
-              points: points,
-              zones: [
-                GeoZone(id: 'z1', points: [LatLng(-23.5480, -46.6500), LatLng(-23.5400, -46.6300), LatLng(-23.5600, -46.6200), LatLng(-23.5700, -46.6450)]),
-              ],
-            ),
-          ],
+        child: MapboxMap(
+          initialCameraPosition: const CameraPosition(target: LatLng(-23.55052, -46.633308), zoom: 10),
+          accessToken: StringsConst().mapbox,
         ),
       ),
     );
